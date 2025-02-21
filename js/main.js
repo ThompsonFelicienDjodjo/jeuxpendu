2b386996a10c1211719a78c9220052523a25a835
thomp <thompsondjodjo021@gmail.com>
2025-02-21 11:40:33 +0100

    Sélection des mots et génération des lettres

--- js/main.js
+++ js/main.js
@@ -5,12 +5,13 @@ const els ={
 };
 
 const words = [
-    'JavaScript',
-    'Language',
-    'Bootstrap',
+    'JavaScript', // words [0]
+    'Language', // words [1]
+    'Bootstrap', // words[2]
 ];
 
-let words = '';
+let choices = [];
+let word = '';
 
 
 const init =() => {
@@ -22,8 +23,11 @@ const init =() => {
 
     // Actions définit pour faire le pendu:
     //Choisir un mot
-    
+    word = pickWord ();
+    console.log('word', word);
     //Générer toutes les lettres pour avoir le choix
+    choices = generateChoices ();
+    console.log(choices);
     //Afficher les mots
     //Afficher les choix
     //Afficher les erreurs
@@ -31,6 +35,28 @@ const init =() => {
 
     // Vérifier la lettre: si elle ne fait pas partis des mots ajouter aux erreurs dans le cas contraire on affiche la lettre du mot
 };
+
+const generateChoices = () => {
+    const choices = [];
+    for (let index = 65; index <= 90; index++) {
+        choices.push(String.fromCharCode(index));
+    }
+    return choices;
+};
+
+const pickWord = () => {
+    const randomIndex = getRandomInt(0, words.length - 1);
+
+    return words[randomIndex];
+};
+
 window.addEventListener('load', () => {
     init()
 })
+
+
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min+1)) + min;
+}
